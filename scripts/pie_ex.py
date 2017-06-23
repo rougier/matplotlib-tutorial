449e2791523bfa0c252c8ed9aed4275789ec50cb
Jaroslaw Kolosowski <jarokolosowski@gmail.com>
2017-06-23 13:31:56 +0200

    Add edges in the pie chart exercise
    Edges can be specified with a dictionary of arguments passed to the
    wedgeprops keyword.

--- scripts/pie_ex.py
+++ scripts/pie_ex.py
@@ -9,9 +9,10 @@ n = 20
 Z = np.ones(n)
 Z[-1] *= 2
 
-plt.axes([0.025,0.025,0.95,0.95])
+plt.axes([0.025, 0.025, 0.95, 0.95])
 
-plt.pie(Z, explode=Z*.05, colors = ['%f' % (i/float(n)) for i in range(n)])
+plt.pie(Z, explode=Z*.05, colors=['%f' % (i/float(n)) for i in range(n)],
+        wedgeprops={"linewidth": 1, "edgecolor": "black"})
 plt.gca().set_aspect('equal')
 plt.xticks([]), plt.yticks([])
 
