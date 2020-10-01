2eead12afacf8fe4e6f381ed76cc8d4bfbba3140
Arjunvankani <65284186+Arjunvankani@users.noreply.github.com>
2020-10-02 01:07:06 +0530

    Improve and give two example of matplot
    Better explanation of wave

--- scripts/exercice_1.py
+++ scripts/exercice_1.py
@@ -4,6 +4,9 @@
 # -----------------------------------------------------------------------------
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+from matplotlib.colors import ListedColormap, BoundaryNorm
 
 X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
 C,S = np.cos(X), np.sin(X)
@@ -11,4 +14,75 @@ C,S = np.cos(X), np.sin(X)
 plt.plot(X,C)
 plt.plot(X,S)
 
+plt.show()
+
+print('Second Example')
+
+fig, (ax1, ax2) = plt.subplots(2, 1)
+# make a little extra space between the subplots
+fig.subplots_adjust(hspace=0.5)
+
+dt = 0.01
+t = np.arange(0, 30, dt)
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+
+nse1 = np.random.randn(len(t))                 # white noise 1
+nse2 = np.random.randn(len(t))                 # white noise 2
+r = np.exp(-t / 0.05)
+
+cnse1 = np.convolve(nse1, r, mode='same') * dt   # colored noise 1
+cnse2 = np.convolve(nse2, r, mode='same') * dt   # colored noise 2
+
+# two signals with a coherent part and a random part
+s1 = 0.01 * np.sin(2 * np.pi * 10 * t) + cnse1
+s2 = 0.01 * np.sin(2 * np.pi * 10 * t) + cnse2
+
+ax1.plot(t, s1, t, s2)
+ax1.set_xlim(0, 5)
+ax1.set_xlabel('time')
+ax1.set_ylabel('s1 and s2')
+ax1.grid(True)
+
+cxy, f = ax2.csd(s1, s2, 256, 1. / dt)
+ax2.set_ylabel('CSD (db)')
+plt.show()
+
+print('Third Example ')
+
+x = np.linspace(0, 3 * np.pi, 500)
+y = np.sin(x)
+dydx = np.cos(0.5 * (x[:-1] + x[1:]))  # first derivative
+
+# Create a set of line segments so that we can color them individually
+# This creates the points as a N x 1 x 2 array so that we can stack points
+# together easily to get the segments. The segments array for line collection
+# needs to be (numlines) x (points per line) x 2 (for x and y)
+points = np.array([x, y]).T.reshape(-1, 1, 2)
+segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+fig, axs = plt.subplots(2, 1, sharex=True, sharey=True)
+
+# Create a continuous norm to map from data points to colors
+norm = plt.Normalize(dydx.min(), dydx.max())
+lc = LineCollection(segments, cmap='viridis', norm=norm)
+# Set the values used for colormapping
+lc.set_array(dydx)
+lc.set_linewidth(2)
+line = axs[0].add_collection(lc)
+fig.colorbar(line, ax=axs[0])
+
+# Use a boundary norm instead
+cmap = ListedColormap(['r', 'g', 'b'])
+norm = BoundaryNorm([-1, -0.5, 0.5, 1], cmap.N)
+lc = LineCollection(segments, cmap=cmap, norm=norm)
+lc.set_array(dydx)
+lc.set_linewidth(2)
+line = axs[1].add_collection(lc)
+fig.colorbar(line, ax=axs[1])
+
+axs[0].set_xlim(x.min(), x.max())
+axs[0].set_ylim(-1.1, 1.1)
 plt.show()
